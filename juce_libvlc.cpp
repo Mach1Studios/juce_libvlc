652bfdb0c23d6d3cd8b8a19c1e4fe7f52d020d7b
Dylan Marcus <dylan@dylanjmarcus.com>
2025-11-11 14:10:19 -0500

    empty module container for linking

--- juce_libvlc.cpp
+++ juce_libvlc.cpp
@@ -2,3 +2,12 @@
 
 // Implementation files are included here for the JUCE module system
 #include "juce_media/VLCMediaPlayer.cpp"
+
+// VLC static module stub
+// When linking statically against libVLC, it expects a vlc_static_modules array.
+// Since we're using dynamic plugin loading (plugins are .dylib files), we provide
+// an empty NULL-terminated array to satisfy the linker.
+extern "C" {
+    typedef int (*vlc_plugin_cb)(void *);
+    vlc_plugin_cb vlc_static_modules[] = { nullptr };
+}
